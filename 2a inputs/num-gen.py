690f063ec6fd13728fa17c54eb54005a3c684d49
Sasi san <worksasikiran@gmail.com>
2025-07-23 23:22:17 +0530

    Delete 2a inputs directory

--- 2a inputs/num-gen.py
+++
@@ -1,6 +0,0 @@
-import random
-for n in [5000,8000,10000,13000,15000,18000,20000,23000,25000,28000,30000,35000]:
-    arr=[random.randint(-10000,10000)for _ in range(n)]
-    with open(f"random_{n}.txt","w")as f:
-        f.write(str(n)+" ")
-        f.write(" ".join(str(x)for x in arr))
